53b6091d96ff2015a00bdfe1dc295f050761bd43
nolanborz <nolanrborzoni@gmail.com>
2024-11-28 19:03:55 +0900

    Remove skip-routes error and add routes

---
+++ test/controllers/flights_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class FlightsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get flights_index_url
+    assert_response :success
+  end
+
+  test "should get skip-routes" do
+    get flights_skip-routes_url
+    assert_response :success
+  end
+end
