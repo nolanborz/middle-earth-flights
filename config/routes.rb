fc906cb1d3702b38b1e71b7f24f43f977e256a9d
nolanborz <nolanrborzoni@gmail.com>
2024-12-14 10:57:30 +0900

    Repair to full functionality no tailwindcss

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "flights#index"
   resources :flights
-  resources :bookings, only: [ :new ]
+  resources :bookings
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
